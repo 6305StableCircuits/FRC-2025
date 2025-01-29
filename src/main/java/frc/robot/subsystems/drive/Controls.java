089902d3afbe85f3240ff2518749b97965e58e0b
noahbedouch <59329993+noahbedouch@users.noreply.github.com>
2025-01-28 18:36:13 -0800

    update but still no work :(

--- src/main/java/frc/robot/subsystems/drive/Controls.java
+++ src/main/java/frc/robot/subsystems/drive/Controls.java
@@ -1,6 +1,16 @@
 package frc.robot.subsystems.drive;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -13,7 +23,8 @@ public class Controls extends Subsystem {
     Drive swerve = Drive.getInstance();
     Limelight limelight = Limelight.getInstance();
 
-    ProfiledPIDController controller = new ProfiledPIDController(2, 0.02, 0, new TrapezoidProfile.Constraints(5, 10));
+    //ProfiledPIDController controller = new ProfiledPIDController(0.3, 0, 0, new TrapezoidProfile.Constraints(5, 10));
+    HolonomicDriveController controller = new HolonomicDriveController(new PIDController(0.05, 0, 0), new PIDController(0.05, 0, 0), new ProfiledPIDController(0.05, 0, 0, new TrapezoidProfile.Constraints(6.28, 3.14)));
     double kP = 0.065;
     double kI = 0.05;
     double kD = 0;
@@ -22,7 +33,10 @@ public class Controls extends Subsystem {
     double errorSum = 0;
     double[] pose;
     double d;
-    double vel;
+    ChassisSpeeds vel;
+    double poseX,poseY;
+    int i = 0;
+    Pigeon2 pigeon = new Pigeon2(45);
 
     public static Controls instance = null;
     public static Controls getInstance() {
@@ -34,29 +48,44 @@ public class Controls extends Subsystem {
 
     public void update() {
         swerve.swerve(joystick);
-        if(joystick.rightBumper().getAsBoolean()) {
-            if(limelight.getLock()) {
-                // if((Math.abs(limelight.getXOffset())) != 0) {
-                //     errorSum += (limelight.getXOffset() * 0.02);
-                //     double output = (limelight.getXOffset() * kP) + (((limelight.getXOffset() - prevError) / 0.02) * kD) + (errorSum * kI);
-                //     System.out.println(output);
-                //     double velocity = Math.max(Math.min(output, 4.33), -4.33);
-                //     swerve.adjust(-velocity);
-                // } else {
-                //     errorSum = 0;
-                // }
-                // prevError = limelight.getXOffset();
-                vel = controller.calculate(0, d);
-                swerve.adjust(-(vel * Math.cos(limelight.getXOffset())), -(vel * Math.sin(limelight.getXOffset())));
-            } // else {
-            //     errorSum = 0;
-            // }
+        // if(joystick.rightBumper().getAsBoolean()) {
+        //     if(limelight.getLock()) {
+        //         // if((Math.abs(limelight.getXOffset())) != 0) {
+        //         //     errorSum += (limelight.getXOffset() * 0.02);
+        //         //     double output = (limelight.getXOffset() * kP) + (((limelight.getXOffset() - prevError) / 0.02) * kD) + (errorSum * kI);
+        //         //     System.out.println(output);
+        //         //     double velocity = Math.max(Math.min(output, 4.33), -4.33);
+        //         //     swerve.adjust(-velocity);
+        //         // } else {
+        //         //     errorSum = 0;
+        //         // }
+        //         // prevError = limelight.getXOffset();
+        //         // vel = controller.calculate(0, d);
+        //         // double velx = vel * (poseX / d);
+        //         // double vely = vel * ((poseY - 0.2) / d);
+        //         // swerve.adjust(-velx, -vely);
+        //     } // else {
+        //     //     errorSum = 0;
+        //     // }
+        // }
+        if(joystick.a().getAsBoolean()) {
+            Rotation2d rot = new Rotation2d(pigeon.getYaw(true).getValueAsDouble());
+            Rotation2d rot2 = new Rotation2d();
+            Pose2d pos = new Pose2d();
+            Pose2d pos2 = new Pose2d(poseX, (poseY - 0.2), rot);
+            var config = new TrajectoryConfig(0, 0);
+            var trajectory = TrajectoryGenerator.generateTrajectory(pos, null, pos2, config);
+
+            vel = controller.calculate(pos, pos2, 1.0, rot2);
+            swerve.adjust(vel.vxMetersPerSecond, vel.vyMetersPerSecond);
         }
     }
 
     public void readPeriodicInputs() {
         pose = limelight.getPose();
-        d = Math.sqrt(Math.pow(pose[0], 2) + Math.pow(pose[3], 2));
+        poseX = pose[0];
+        poseY = pose[3];
+        d = Math.sqrt(Math.pow(poseX, 2) + Math.pow(poseY, 2));
     }
 
     @Override
