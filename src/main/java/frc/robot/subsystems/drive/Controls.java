7c8ef81225b7f84d8df7c82ebc82ebd6a9b0ee9c
Noah Bedouch <noahbedouch@gmail.com>
2025-02-06 00:26:46 -0800

    pushing to prod, see you next week (pt. 2)

--- src/main/java/frc/robot/subsystems/drive/Controls.java
+++ src/main/java/frc/robot/subsystems/drive/Controls.java
@@ -45,6 +45,7 @@ public class Controls extends Subsystem {
     TrajectoryConfig config;
     Trajectory trajectory;
     boolean trajectoryGenerated = false;
+    PathPlannerPath path;
 
     public static Controls instance = null;
     public static Controls getInstance() {
@@ -65,48 +66,45 @@ public class Controls extends Subsystem {
         if(joystick.a().getAsBoolean()) {
             if(!trajectoryGenerated) {
                 generateTrajectory();
-                timer.start();
                 trajectoryGenerated = true;
             } else {
-                followTrajectory();
+                swerve.followPathCommand(path);
             }
             if(trajectory.getTotalTimeSeconds() >= timer.get()) {
-                timer.restart();
                 trajectoryGenerated = false;
             }
         }
     }
 
-    public void generateTrajectory() {
-        rot = new Rotation2d(yaw);
-        rot2 = new Rotation2d();
-        desPos = new Pose2d(0, 0, rot2);
-        curPos = new Pose2d(poseX, poseY, rot);
-        mid = new Translation2d((poseX / 2), (poseY / 2));
-        config = new TrajectoryConfig(3, 2);
-        trajectory = TrajectoryGenerator.generateTrajectory(curPos, List.of(mid), desPos, config);
-        System.out.println("Trajectory: " + trajectory.sample(timer.get()));
-    }
+    // public void generateTrajectory() {
+    //     rot = new Rotation2d(yaw);
+    //     rot2 = new Rotation2d();
+    //     desPos = new Pose2d(0, 0, rot2);
+    //     curPos = new Pose2d(poseX, poseY, rot);
+    //     mid = new Translation2d((poseX / 2), (poseY / 2));
+    //     config = new TrajectoryConfig(3, 2);
+    //     trajectory = TrajectoryGenerator.generateTrajectory(curPos, List.of(mid), desPos, config);
+    //     System.out.println("Trajectory: " + trajectory.sample(timer.get()));
+    // }
 
-    public void pathPlannerStuff() {
+    public void generateTrajectory() {
         List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
             curPos,
             desPos
         );
         PathConstraints constraints = PathConstraints.unlimitedConstraints(12.0);
-        @SuppressWarnings("unused") //viola ✨
-        PathPlannerPath path = new PathPlannerPath(
+        path = new PathPlannerPath(
             waypoints, constraints, null, new GoalEndState(0.0, rot2));
     }
 
-    public void followTrajectory() {
-        Rotation2d newRot = new Rotation2d(yaw);
-        Pose2d curPos2 = new Pose2d(poseX, poseY, newRot);
-        System.out.println(trajectory);
-        vel = controller.calculate(curPos2, trajectory.sample(timer.get()), rot2);
-        //System.out.println(curPos2);
-        //swerve.adjust(-vel.vxMetersPerSecond, vel.vyMetersPerSecond, vel.omegaRadiansPerSecond);
-    }
+    // public void followTrajectory() {
+    //     Rotation2d newRot = new Rotation2d(yaw);
+    //     Pose2d curPos2 = new Pose2d(poseX, poseY, newRot);
+    //     System.out.println(trajectory);
+    //     vel = controller.calculate(curPos2, trajectory.sample(timer.get()), rot2);
+    //     //System.out.println(curPos2);
+    //     //swerve.adjust(-vel.vxMetersPerSecond, vel.vyMetersPerSecond, vel.omegaRadiansPerSecond);
+    // }
 
     public void readPeriodicInputs() {
         pose = limelight.getPose();
