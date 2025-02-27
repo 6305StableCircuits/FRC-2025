6b2cce7c58b356d1da50d390e045c598c6dff839
noahbedouch <59329993+noahbedouch@users.noreply.github.com>
2025-02-26 21:18:19 -0800

    womp womp bad pid lol

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,9 +2,11 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -25,10 +27,11 @@ public class Elevator extends Subsystem {
     public double hunterOffset,garrettOffset;
     Slot0Configs hunterSlot0Configs = new Slot0Configs();
     Slot0Configs garrettSlot0Configs = new Slot0Configs();
-    final TrapezoidProfile hunterProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(1.0, 0.5));
-    final TrapezoidProfile garrettProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(1, 0.5));
+    final TrapezoidProfile hunterProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(0.5, 0.0625));
+    final TrapezoidProfile garrettProfile = new TrapezoidProfile(new TrapezoidProfile.Constraints(0.5, 0.0625));
     final PositionVoltage hunterRequest = new PositionVoltage(0).withSlot(0);
-    final PositionVoltage garrettRequest = new PositionVoltage(0).withSlot(0);
+    //final PositionVoltage garrettRequest = new PositionVoltage(0).withSlot(0);
+    final Follower garrettRequest = new Follower(10, true);
     
     public static Elevator instance = null;
     public static Elevator getInstance() {
@@ -47,15 +50,20 @@ public class Elevator extends Subsystem {
         // garrettFeedForward = new ElevatorFeedforward(0, 0.5,0);
         hunter = new TalonFX(Constants.hunterID, "Canviore");
         garrett = new TalonFX(Constants.garrettID, "Canviore");
-        hunterSlot0Configs.kG = 0.5;
-        garrettSlot0Configs.kG = 0.5;
-        hunterSlot0Configs.kP = 1;
-        garrettSlot0Configs.kP = 1;
+        hunterSlot0Configs.kG = 1.5;
+        garrettSlot0Configs.kG = 1.5;
+        hunterSlot0Configs.kP = 50;
+        garrettSlot0Configs.kP = 50;
+        hunterSlot0Configs.kS = 1.5;
+        garrettSlot0Configs.kS = 1.5;
+        hunterSlot0Configs.kI = 10;
+        garrettSlot0Configs.kI = 10;
+        // hunterSlot0Configs.kD = 0.5;
+        // garrettSlot0Configs.kD = 0.5;
         limitSwitch = new DigitalInput(0);
         // hunterOffset = hunter.getPosition().getValueAsDouble();
         // garrettOffset = garrett.getPosition().getValueAsDouble();
         hunter.getConfigurator().apply(hunterSlot0Configs);
-        garrett.getConfigurator().apply(garrettSlot0Configs);
     }
 
     public double heightToMotorDegrees(double heightRequest) {
@@ -67,18 +75,18 @@ public class Elevator extends Subsystem {
         // garrettController.setGoal(5);
         // hunter.setControl(new PositionVoltage(hunterController.calculate(((hunter.getPosition().getValueAsDouble() - hunterOffset))) + hunterFeedForward.calculate(hunterController.getSetpoint().velocity)));
         // garrett.setControl(new PositionVoltage(garrettController.calculate(((garrett.getPosition().getValueAsDouble() - garrettOffset))) + garrettFeedForward.calculate(garrettController.getSetpoint().velocity)));
-        TrapezoidProfile.State hunterGoal = new TrapezoidProfile.State(15, 0);
+        TrapezoidProfile.State hunterGoal = new TrapezoidProfile.State(30, 0);
         TrapezoidProfile.State hunterSetpoint = new TrapezoidProfile.State();
-        TrapezoidProfile.State garrettGoal = new TrapezoidProfile.State(15, 0);
-        TrapezoidProfile.State garrettSetpoint = new TrapezoidProfile.State();
+        //TrapezoidProfile.State garrettGoal = new TrapezoidProfile.State(30, 0);
+        //TrapezoidProfile.State garrettSetpoint = new TrapezoidProfile.State();
 
         hunterSetpoint = hunterProfile.calculate(0.02, hunterSetpoint, hunterGoal);
-        garrettSetpoint = garrettProfile.calculate(0.02, garrettSetpoint, garrettGoal);
+        //garrettSetpoint = garrettProfile.calculate(0.02, garrettSetpoint, garrettGoal);
 
         hunterRequest.Position = hunterSetpoint.position;
         hunterRequest.Velocity = hunterSetpoint.velocity;
-        garrettRequest.Position = garrettSetpoint.position;
-        garrettRequest.Velocity = garrettSetpoint.velocity;
+        // garrettRequest.Position = garrettSetpoint.position;
+        // garrettRequest.Velocity = garrettSetpoint.velocity;
 
         hunter.setControl(hunterRequest);
         garrett.setControl(garrettRequest);
